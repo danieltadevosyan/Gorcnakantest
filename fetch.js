64abd4fdf7c92ac74a85b9dd7f86e648a1287c90
danieltadevosyan <danieltadevosyan95@gmail.com>
2024-02-03 16:47:31 +0400

    array method

--- fetch.js
+++ fetch.js
@@ -16,20 +16,34 @@ class Singleton {
   }
 }
 
-const res = arr.reduce((acc, item) => {
-  if (acc[item.l]) {
-    acc[item.l] = [...acc[item.l], item];
-  } else {
-    acc[item.l] = [item];
-  }
-  return acc;
-}, {});
+// const res = arr.reduce((acc, item) => {
+//   if (acc[item.l]) {
+//     acc[item.l] = [...acc[item.l], item];
+//   } else {
+//     acc[item.l] = [item];
+//   }
+//   return acc;
+// }, {});
 
-console.log(res);
+// console.log(res);
 
-let arr = [
-  { v: "dan", l: "dan" },
-  { v: "goq", l: "goq" },
-  { v: "mis", l: "dan" },
-  { v: "dan", l: "dan" },
-];
+// let arr = [
+//   { v: "dan", l: "dan" },
+//   { v: "goq", l: "goq" },
+//   { v: "mis", l: "dan" },
+//   { v: "dan", l: "dan" },
+// ];
+
+Array.prototype.groupBy = function (id) {
+  let newArr = this.reduce((acc, item) => {
+    if (acc[item[id]]) {
+      acc[item[id]] = [...acc[item[id]], item];
+    } else {
+      acc[item[id]] = [item];
+    }
+    return acc;
+  }, {});
+  return newArr;
+};
+
+console.log(arr.groupBy("v"));
