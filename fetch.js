38ea146b12860c90c191384a6a4f9c5c406295f7
danieltadevosyan <danieltadevosyan95@gmail.com>
2024-02-03 16:38:56 +0400

    class added

--- fetch.js
+++ fetch.js
@@ -2,3 +2,16 @@
 // private methodin avelacnel fetch
 // stexcel array method vory group kani stcac datan
 //
+
+class Singleton {
+  #fetchData(url) {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.groupBy("userId"));
+      });
+  }
+  showData(url) {
+    return this.#fetchData(url);
+  }
+}
